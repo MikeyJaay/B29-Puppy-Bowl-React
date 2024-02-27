fc9be17b311653bc7de997d6ad86baa43588cd3d
Michael  Jaroszynski <mikeyj@Mikey-Js-MacBook-Pro.local>
2024-02-27 13:51:43 -0500

    added form to add new players

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import AllPlayers from "./components/AllPlayers";
 import SinglePlayer from './components/SinglePlayer'
+import NewPlayerForm from "./components/NewPlayerForm";
 
 import './App.css'
 import NavBar from './components/Navbar'
@@ -9,10 +10,13 @@ function App() {
   return (
     <>
       <NavBar />
+      <main>
       <Routes>
           <Route path='/' element={<AllPlayers/>} />
           <Route path='/players/:id' element={<SinglePlayer />} />
+          <Route path='/NewPlayerForm' element={<NewPlayerForm />} />
       </Routes>
+      </main>
     </>
   )
 }
